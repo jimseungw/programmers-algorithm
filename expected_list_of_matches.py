a9caa89bba2e4810de6c23cd487f36f4178779ea
jimseungw <jin6491@gmail.com>
2018-08-15 15:04:04 +0900

    ....

---
+++ expected_list_of_matches.py
@@ -0,0 +1,8 @@
+import math
+def solution(n,a,b):
+    a, b = min(a,b), max(a,b)
+    for k in range(1, int(math.log2(n)+1)):
+        if a%2 != 0 and a+1 == b:
+            return k
+        else:
+            n, a, b = n/2, math.ceil(a/2), math.ceil(b/2)
